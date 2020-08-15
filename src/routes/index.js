afc1805b301fa07e592618de28a49fca5d515fe5
olehhapuk <oleggapuk20@gmail.com>
2020-08-15 11:46:06 +0300

    feat: change POST /authentication to POSTT /authenticate

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const users = require('./users');
 
 const router = express.Router();
 
-router.use('/authentication', authentication);
+router.use('/authenticate', authentication);
 router.use('/users', users);
 
 module.exports = router;
