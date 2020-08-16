58c75c2ab52652452d36b2fe9b1f5090a0642d26
olehhapuk <oleggapuk20@gmail.com>
2020-08-16 11:07:20 +0300

    feat: reauthenticate

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-const { InvalidInput, InvalidLogin } = require('../errors');
+const { InvalidInput, InvalidLogin, InternalServerError } = require('../errors');
 const models = require('../db/models');
 
 const router = express.Router();
@@ -20,44 +20,77 @@ function signJwtToken(payload) {
   });
 }
 
+function verifyJwtToken(token) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_KEY, (error, payload) => {
+      if (error) reject(error);
+      else {
+        resolve(payload);
+      }
+    });
+  })
+}
+
 router.post('/', async (req, res, next) => {
-  const userInput = {
-    username: req.body.username,
-    password: req.body.password
-  };
+  switch (req.body.strategy) {
+    case 'local':
+      const userInput = {
+        username: req.body.username,
+        password: req.body.password
+      };
+    
+      if (!userInput.username || !userInput.password) {
+        InvalidInput(res, next);
+      }
+    
+      try {
+        const user = await User.findOne({ username: userInput.username });
+    
+        if (!user) {
+          InvalidLogin(res, next);
+        }
+        else {
+          const passMatch = await bcrypt.compare(userInput.password, user.password);
+    
+          if (passMatch) {
+            // generate JWT token and send it back
+            const payload = JSON.parse(JSON.stringify(user));
+            delete payload.password;
+    
+            const token = await signJwtToken(payload);
+    
+            const responseMessage = {
+              accessToken: token,
+              user: payload
+            };
+            res.json(responseMessage);
+          }
+          else {
+            InvalidLogin(res, next);
+          }
+        }
+      } catch (error) {
+        InvalidLogin(res, next, error);
+      }
+      break;
 
-  if (!userInput.username || !userInput.password) {
-    InvalidInput(res, next);
-  }
-
-  try {
-    const user = await User.findOne({ username: userInput.username });
-
-    if (!user) {
-      InvalidLogin(res, next);
-    }
-    else {
-      const passMatch = await bcrypt.compare(userInput.password, user.password);
-
-      if (passMatch) {
-        // generate JWT token and send it back
-        const payload = JSON.parse(JSON.stringify(user));
-        delete payload.password;
-
-        const token = await signJwtToken(payload);
-
+    case 'JWT':
+      try {
+        const userToken = req.headers.authorization.split(' ')[1];
+        const payload = await verifyJwtToken(userToken);
         const responseMessage = {
-          accessToken: token,
+          accessToken: userToken,
           user: payload
         };
         res.json(responseMessage);
+      } catch (error) {
+        InternalServerError(res, next, error);
       }
-      else {
-        InvalidLogin(res, next);
-      }
-    }
-  } catch (error) {
-    InvalidLogin(res, next, error);
+      break;
+  
+    default:
+      InvalidInput(res, next);
+      break;
   }
 });
 
