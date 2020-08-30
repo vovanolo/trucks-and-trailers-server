6254ffe91a56a8993b8c5fb3aa00ade9d6db489b
olehhapuk <oleggapuk20@gmail.com>
2020-08-30 17:02:38 +0300

    feat: added trucks route to /api/trucks

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const authentication = require('./authentication');
 const users = require('./users');
 const drivers = require('./drivers');
 const trailers = require('./trailers');
+const trucks = require('./trucks');
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/auth', authentication);
 router.use('/users', users);
 router.use('/drivers', drivers);
 router.use('/trailers', trailers);
+router.use('/trucks', trucks);
 
 module.exports = router;
